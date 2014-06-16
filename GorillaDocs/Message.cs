7d7d634a7ad6ccb7328e5902782e80ec2f5d5147
Matthew Fitzmaurice <matthew.fitzmaurice@gmail.com>
2014-06-16 23:56:34 +0800

    Added PostSharp & Log4Net support

---
+++ GorillaDocs/Message.cs
@@ -0,0 +1,130 @@
+﻿using log4net;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GorillaDocs
+{
+    public class Message
+    {
+        public static void Configure(FileInfo ConfigFile, string LogLevel)
+        {
+            log4net.Config.XmlConfigurator.Configure(ConfigFile);
+            var repository = ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository());
+            repository.Root.Level = repository.LevelMap[LogLevel];
+            repository.RaiseConfigurationChanged(EventArgs.Empty);
+        }
+
+        //See http://logging.apache.org/log4net/index.html
+        public static void ShowError(Exception ex)
+        {
+            StackTrace stackTrace = new StackTrace();
+            MethodBase method = stackTrace.GetFrame(1).GetMethod();
+            MessageBox.Show(ex.Message, String.Format("{0} {1}", Assembly.GetExecutingAssembly().Title(), Assembly.GetExecutingAssembly().FileVersion()), MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            LogError(method, ex);
+        }
+
+        public static void ShowInformation(string message)
+        {
+            MessageBox.Show(message, String.Format("{0} {1}", Assembly.GetExecutingAssembly().Title(), Assembly.GetExecutingAssembly().FileVersion()), MessageBoxButton.OK, MessageBoxImage.Information);
+            LogInfo(message);
+        }
+
+        public static void LogError(Exception ex)
+        {
+            StackTrace stackTrace = new StackTrace();
+            MethodBase method = stackTrace.GetFrame(1).GetMethod();
+            LogError(method, ex);
+        }
+        static void LogError(MethodBase method, Exception ex)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ILog log = LogManager.GetLogger(method.DeclaringType);
+                log.Error(method.Name, ex);
+            });
+        }
+
+        [System.Diagnostics.DebuggerStepThrough]
+        public static bool IsWarnEnabled()
+        {
+            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            return log.IsWarnEnabled;
+        }
+
+        public static void LogWarning(Exception ex)
+        {
+            StackTrace stackTrace = new StackTrace();
+            MethodBase method = stackTrace.GetFrame(1).GetMethod();
+            LogWarning(method, string.Empty, ex);
+        }
+        public static void LogWarning(MethodBase method, Exception ex)
+        {
+            LogWarning(method, string.Empty, ex);
+        }
+        [System.Diagnostics.DebuggerStepThrough]
+        public static void LogWarning(MethodBase method, string parameters, Exception ex)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ILog log = LogManager.GetLogger(method.DeclaringType);
+                log.Warn(method.Name + parameters, ex);
+            });
+        }
+        public static void LogWarning(string value)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                StackTrace stackTrace = new StackTrace();
+                MethodBase method = stackTrace.GetFrame(1).GetMethod();
+                ILog log = LogManager.GetLogger(method.DeclaringType);
+                log.Warn(value);
+            });
+        }
+        public static void LogWarning(string format, params object[] args)
+        {
+            LogWarning(string.Format(format, args));
+        }
+
+        public static void LogInfo(string value)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                StackTrace stackTrace = new StackTrace();
+                MethodBase method = stackTrace.GetFrame(1).GetMethod();
+                ILog log = LogManager.GetLogger(method.DeclaringType);
+                log.InfoFormat("{0} {1}", method.Name, value);
+            });
+        }
+        public static void LogInfo(string format, params object[] args)
+        {
+            LogInfo(string.Format(format, args));
+        }
+
+        [System.Diagnostics.DebuggerStepThrough]
+        public static bool IsDebugEnabled()
+        {
+            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            return log.IsDebugEnabled;
+        }
+
+        public static void LogDebug(string value)
+        {
+            StackTrace stackTrace = new StackTrace();
+            MethodBase method = stackTrace.GetFrame(1).GetMethod();
+            LogDebug(method, value);
+        }
+        [System.Diagnostics.DebuggerStepThrough]
+        public static void LogDebug(MethodBase method, string value)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ILog log = LogManager.GetLogger(method.DeclaringType);
+                if (log.IsDebugEnabled) log.DebugFormat("{0} {1}", method.Name, value);
+            });
+        }
+    }
+}
---
+++ GorillaDocs/libs/PostSharp/LogAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PostSharp.Aspects;
+
+namespace GorillaDocs.libs.PostSharp
+{
+    [Serializable]
+    public sealed class LogAttribute : OnMethodBoundaryAspect
+    {
+        [System.Diagnostics.DebuggerStepThrough]
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            if (Message.IsDebugEnabled())
+                Message.LogDebug(args.Method, string.Format("{0} Enter", GetParameters(args)));
+        }
+
+        [System.Diagnostics.DebuggerStepThrough]
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            if (Message.IsDebugEnabled())
+                Message.LogDebug(args.Method, "Returns: " + args.ReturnValue);
+        }
+
+        [System.Diagnostics.DebuggerStepThrough]
+        public override void OnException(MethodExecutionArgs args)
+        {
+            if (Message.IsWarnEnabled())
+                Message.LogWarning(args.Method, args.Exception);
+        }
+
+        [System.Diagnostics.DebuggerStepThrough]
+        string GetParameters(MethodExecutionArgs args)
+        {
+            string parameters = string.Empty;
+            for (var i = 0; i < args.Arguments.Count; i++)
+            {
+                string name = args.Method.GetParameters()[i].Name;
+                object arg = args.Arguments[i];
+                Type type = arg.NullableGetType();
+                string value = Convert.ToString(arg);
+                if (Convert.ToString(type) == value)
+                    parameters += string.Format(", {0} [{1}]", name, type);
+                else
+                    parameters += string.Format(", {0} [{1}] {2}", name, type, value);
+            }
+            return string.Format("({0})", parameters.TrimStart(new char[] { ',', ' ' }));
+        }
+    }
+}
