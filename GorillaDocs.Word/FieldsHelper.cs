acca0ae1e7ee690bf4e7b7f1d6015ee00739f720
Matthew Fitzmaurice <matthew.fitzmaurice@gmail.com>
2014-10-01 15:41:51 +0800

    Strong name Word dll and more extension methods

--- GorillaDocs.Word/FieldsHelper.cs
+++ GorillaDocs.Word/FieldsHelper.cs
@@ -1,4 +1,5 @@
 ﻿using GorillaDocs.libs.PostSharp;
+using System;
 using O = Microsoft.Office.Core;
 using Wd = Microsoft.Office.Interop.Word;
 
@@ -7,6 +8,43 @@ namespace GorillaDocs.Word
     [Log]
     public static class FieldsHelper
     {
+        delegate void FieldActionDelegate(Wd.Field field);
+        delegate void RangeActionDelegate(Wd.Range range);
+        delegate void FieldTypeActionDelegate(Wd.Range range, Wd.WdFieldType type, string name);
+
+        public static void UpdateAllFields(this Wd.Document doc)
+        {
+            doc.ProcessAllRanges(UpdateAllFields);
+        }
+        public static void UnlinkAllFields(this Wd.Document doc)
+        {
+            doc.ProcessAllRanges(UnlinkAllFields);
+        }
+        public static void UpdateFields(this Wd.Document doc, string name, Wd.WdFieldType type)
+        {
+            doc.ProcessAllRanges(type, name, UpdateFields);
+        }
+        public static void UnlinkFields(this Wd.Document doc, Wd.WdFieldType type, string name = null)
+        {
+            doc.ProcessAllRanges(type, name, UnlinkFields);
+        }
+
+        public static void UpdateAllFields(this Wd.Range range)
+        {
+            range.Fields.Update();
+        }
+        public static void UnlinkAllFields(this Wd.Range range)
+        {
+            range.Fields.Unlink();
+        }
+        public static void UpdateFields(this Wd.Range range, Wd.WdFieldType type, string name)
+        {
+            ProcessFields(range, type, name, UpdateField);
+        }
+        public static void UnlinkFields(this Wd.Range range, Wd.WdFieldType type, string name = null)
+        {
+            ProcessFields(range, type, name, UnlinkField);
+        }
         public static void UpdateFields(this Wd.HeaderFooter headerfooter)
         {
             headerfooter.Range.Fields.Update();
@@ -19,40 +57,135 @@ namespace GorillaDocs.Word
             }
         }
 
-        public static void UpdateAllFields(this Wd.Document doc)
+        public static bool ContainsField(this Wd.Range range, Wd.WdFieldType type, string name)
         {
-            doc.Fields.Update();
+            Wd.Range result = range.Find(type, name);
+            return result.Find.Found;
+        }
+        public static void DeleteFields(this Wd.Range range, Wd.WdFieldType type, string name, bool DeleteEmptyParagraphs = true)
+        {
+            Wd.Range search = range.Duplicate;
+            Wd.Range result = search.Find(type, name);
+            while (result.Find.Found)
+            {
+                result.Delete();
+                if (result.Paragraphs[1].Range.Characters.Count == 1 && DeleteEmptyParagraphs)
+                    result.Paragraphs[1].Range.Delete();
+
+                search.Start = result.End;
+                result = search.Find(type, name);
+            }
+        }
+        public static Wd.Field GetField(this Wd.Range range, Wd.WdFieldType type, string name)
+        {
+            Wd.Range search = range.Duplicate;
+            Wd.Range result = search.Find(type, name);
+            if (result.Find.Found)
+                return result.Fields[1];
+            else
+                return null;
+        }
+
+        public static string AsGoToString(this Wd.WdFieldType type)
+        {
+            switch (type)
+            {
+                case Wd.WdFieldType.wdFieldDocProperty:
+                    return "DOCPROPERTY";
+                case Wd.WdFieldType.wdFieldDocVariable:
+                    return "DOCVARIABLE";
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        static void ProcessAllRanges(this Wd.Document doc, Wd.WdFieldType type, string name, FieldTypeActionDelegate action)
+        {
+            //TODO: Change to use StoryRanges
+
+            action(doc.Range(), type, name);
             foreach (Wd.Section section in doc.Sections)
             {
                 foreach (Wd.HeaderFooter header in section.Headers)
                     if (header.Exists)
-                        header.Range.Fields.Update();
+                        action(header.Range, type, name);
                 foreach (Wd.HeaderFooter footer in section.Footers)
                     if (footer.Exists)
-                        footer.Range.Fields.Update();
+                        action(footer.Range, type, name);
             }
-            //NOTE: Can not use For Each loop ..
-            //For some reason, infinite loop results on some machines..
+            //NOTE: Can not use For Each loop on Bakers machines..
+            //Works fine at MacroView. For some reason, infinite loop results at Bakers..
             //Change to standard For loop fixes Word bug.
             for (int i = 1; i <= doc.Shapes.Count; i++)
             {
                 Wd.Shape shape = doc.Shapes[i];
                 if (shape.Type == O.MsoShapeType.msoTextBox && shape.TextFrame.HasText == -1)
-                    shape.TextFrame.TextRange.Fields.Update();
+                    action(shape.TextFrame.TextRange, type, name);
             }
             //Update the textboxes in the headers - all the shapes from every section header and footer are available in the first header.
             for (int i = 1; i <= doc.Sections[1].Headers[Wd.WdHeaderFooterIndex.wdHeaderFooterPrimary].Shapes.Count; i++)
             {
                 Wd.Shape shape = doc.Sections[1].Headers[Wd.WdHeaderFooterIndex.wdHeaderFooterPrimary].Shapes[i];
                 if (shape.Type == O.MsoShapeType.msoTextBox && shape.TextFrame.HasText == -1)
-                    shape.TextFrame.TextRange.Fields.Update();
+                    action(shape.TextFrame.TextRange, type, name);
             }
             for (int i = 1; i <= doc.Sections[1].Footers[Wd.WdHeaderFooterIndex.wdHeaderFooterPrimary].Shapes.Count; i++)
             {
                 Wd.Shape shape = doc.Sections[1].Footers[Wd.WdHeaderFooterIndex.wdHeaderFooterPrimary].Shapes[i];
                 if (shape.Type == O.MsoShapeType.msoTextBox && shape.TextFrame.HasText == -1)
-                    shape.TextFrame.TextRange.Fields.Update();
+                    action(shape.TextFrame.TextRange, type, name);
             }
         }
+        static void ProcessAllRanges(this Wd.Document doc, RangeActionDelegate action)
+        {
+            //TODO: Change to use StoryRanges
+
+            action(doc.Range());
+            foreach (Wd.Section section in doc.Sections)
+            {
+                foreach (Wd.HeaderFooter header in section.Headers)
+                    if (header.Exists)
+                        action(header.Range);
+                foreach (Wd.HeaderFooter footer in section.Footers)
+                    if (footer.Exists)
+                        action(footer.Range);
+            }
+            //NOTE: Can not use For Each loop on Bakers machines..
+            //Works fine at MacroView. For some reason, infinite loop results at Bakers..
+            //Change to standard For loop fixes Word bug.
+            for (int i = 1; i <= doc.Shapes.Count; i++)
+            {
+                Wd.Shape shape = doc.Shapes[i];
+                if (shape.Type == O.MsoShapeType.msoTextBox && shape.TextFrame.HasText == -1)
+                    action(shape.TextFrame.TextRange);
+            }
+            //Update the textboxes in the headers - all the shapes from every section header and footer are available in the first header.
+            for (int i = 1; i <= doc.Sections[1].Headers[Wd.WdHeaderFooterIndex.wdHeaderFooterPrimary].Shapes.Count; i++)
+            {
+                Wd.Shape shape = doc.Sections[1].Headers[Wd.WdHeaderFooterIndex.wdHeaderFooterPrimary].Shapes[i];
+                if (shape.Type == O.MsoShapeType.msoTextBox && shape.TextFrame.HasText == -1)
+                    action(shape.TextFrame.TextRange);
+            }
+            for (int i = 1; i <= doc.Sections[1].Footers[Wd.WdHeaderFooterIndex.wdHeaderFooterPrimary].Shapes.Count; i++)
+            {
+                Wd.Shape shape = doc.Sections[1].Footers[Wd.WdHeaderFooterIndex.wdHeaderFooterPrimary].Shapes[i];
+                if (shape.Type == O.MsoShapeType.msoTextBox && shape.TextFrame.HasText == -1)
+                    action(shape.TextFrame.TextRange);
+            }
+        }
+        static void ProcessFields(Wd.Range range, Wd.WdFieldType type, string name, FieldActionDelegate action)
+        {
+            Wd.Range search = range.Duplicate;
+            Wd.Range result = search.Find(type, name);
+            while (result.Find.Found)
+            {
+                action(result.Fields[1]);
+                search.Start = result.End;
+                result = search.Find(type, name);
+            }
+        }
+        static void UpdateField(Wd.Field field) { field.Update(); }
+        static void UnlinkField(Wd.Field field) { field.Unlink(); }
+
     }
 }
---
+++ GorillaDocs.Word/SearchHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wd = Microsoft.Office.Interop.Word;
+
+namespace GorillaDocs.Word
+{
+    public static class SearchHelper
+    {
+        public static Wd.Range Find(this Wd.Range range, string value)
+        {
+            var ShowAll = range.Document.ActiveWindow.View.ShowAll;
+
+            try
+            {
+                range.Document.ActiveWindow.View.ShowAll = true;
+
+                Wd.Range result = range.Duplicate;
+                Wd.Find find = result.Find;
+                find.ClearFormatting();
+                find.Text = value;
+                find.Forward = true;
+                find.Wrap = Wd.WdFindWrap.wdFindStop;
+                find.Forward = true;
+                find.MatchCase = false;
+                find.MatchWholeWord = false;
+                find.MatchWildcards = false;
+                find.MatchSoundsLike = false;
+                find.MatchAllWordForms = false;
+                find.Execute();
+
+                int i = 0;
+                while (result.Find.Found && result.Text != value)
+                {
+                    result = range.Find(value);
+                    if (i++ > 10)
+                        throw new InvalidOperationException(string.Format("The value '{0}' exists in the range, but Word is unable to find it.", value));
+                }
+
+                return result;
+            }
+            finally
+            {
+                range.Document.ActiveWindow.View.ShowAll = ShowAll;
+            }
+        }
+
+        public static Wd.Range Find(this Wd.Range range, Wd.WdFieldType type, string value = null)
+        {
+            var ShowAll = range.Document.ActiveWindow.View.ShowAll;
+            var ShowFieldCodes = range.Document.ActiveWindow.View.ShowFieldCodes;
+
+            try
+            {
+                range.Document.ActiveWindow.View.ShowAll = true;
+                range.Document.ActiveWindow.View.ShowFieldCodes = true;
+                value = string.Format("^d {0}^w{1}", type.AsGoToString(), value);
+
+                Wd.Range result = range.Duplicate;
+                Wd.Find find = result.Find;
+                find.ClearFormatting();
+                find.Text = value;
+                find.Forward = true;
+                find.Wrap = Wd.WdFindWrap.wdFindStop;
+                find.Forward = true;
+                find.MatchCase = false;
+                find.MatchWholeWord = false;
+                find.MatchWildcards = false;
+                find.MatchSoundsLike = false;
+                find.MatchAllWordForms = false;
+                find.Execute();
+
+                return result;
+            }
+            finally
+            {
+                range.Document.ActiveWindow.View.ShowAll = ShowAll;
+                range.Document.ActiveWindow.View.ShowFieldCodes = ShowFieldCodes;
+            }
+        }
+    }
+}
