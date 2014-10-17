88d8a215f5762caa46f9234026c7cca31b2dbb0d
Matthew Fitzmaurice <matthew.fitzmaurice@gmail.com>
2014-10-17 10:06:14 +0800

    Added SaveSchema generic extension method

---
+++ GorillaDocs/SchemaHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace GorillaDocs
+{
+    public static class SchemaHelper
+    {
+        public static void SaveSchema<T>(this T obj, string FullName)
+        {
+            var importer = new XmlReflectionImporter();
+            var schemas = new XmlSchemas();
+            var exporter = new XmlSchemaExporter(schemas);
+            Type type = obj.GetType();
+            XmlTypeMapping map = importer.ImportTypeMapping(type);
+            exporter.ExportTypeMapping(map);
+
+            TextWriter tw = new StreamWriter(FullName);
+            schemas[0].Write(tw);
+            tw.Close();
+        }
+    }
+}
