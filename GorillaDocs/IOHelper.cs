6251b9105202130ca770420c032259a023ce548b
Matthew Fitzmaurice <matthew.fitzmaurice@gmail.com>
2015-02-17 11:08:56 +0800

    Strong Name for SharePoint dll

--- GorillaDocs/IOHelper.cs
+++ GorillaDocs/IOHelper.cs
@@ -5,6 +5,8 @@ using System.Linq;
 
 namespace GorillaDocs
 {
+    public enum OfficeFileType { Word, Excel, PowerPoint, None }
+
     // No LOG Attribute - Can not log because this code is called by the logging code
     public static class IOHelper
     {
@@ -21,9 +23,9 @@ namespace GorillaDocs
             return folder.GetFiles(searchPattern).Where(x => !x.Name.StartsWith("~$")).Count() > 0;
         }
 
-        public static List<FileInfo> GetFilesList(this DirectoryInfo folder, string searchPattern)
+        public static List<FileInfo> GetFilesList(this DirectoryInfo folder, string searchPattern, SearchOption searchOption = SearchOption.AllDirectories)
         {
-            return folder.GetFiles(searchPattern, SearchOption.AllDirectories)
+            return folder.GetFiles(searchPattern, searchOption)
                 .Where(x => !x.Name.StartsWith("~$"))
                 .OrderBy(x => x.Name)
                 .ToList();
@@ -39,6 +41,23 @@ namespace GorillaDocs
         {
             return (file.Extension.ToLower() == ".doc" || file.Extension.ToLower() == ".docx" || file.Extension.ToLower() == ".docm" ||
                 file.Extension.ToLower() == ".dot" || file.Extension.ToLower() == ".dotx" || file.Extension.ToLower() == ".dotm");
+        }
+        public static bool IsExcel(this FileInfo file)
+        {
+            return (file.Extension.ToLower() == ".xls" || file.Extension.ToLower() == ".xlsx" || file.Extension.ToLower() == ".xlsm" ||
+                file.Extension.ToLower() == ".xlt" || file.Extension.ToLower() == ".xltx" || file.Extension.ToLower() == ".xltm");
+        }
+
+        public static OfficeFileType Type(this FileInfo file)
+        {
+            if (file.IsWord())
+                return OfficeFileType.Word;
+            else if (file.IsExcel())
+                return OfficeFileType.Excel;
+            else if (file.IsPowerPoint())
+                return OfficeFileType.PowerPoint;
+            else
+                return OfficeFileType.None;
         }
     }
 }
