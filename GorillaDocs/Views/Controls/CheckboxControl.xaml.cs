c4c0a23ee0ad58c2613ad54ea60b8e7d13f922b6
Matthew Fitzmaurice <matthew.fitzmaurice@gmail.com>
2015-04-05 11:51:20 +0800

    Added form controls

---
+++ GorillaDocs/Views/Controls/CheckboxControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GorillaDocs.Views.Controls
+{
+    public partial class CheckboxControl : UserControl
+    {
+        public CheckboxControl() { InitializeComponent(); }
+
+        public string Label { get; set; }
+        public bool IsChecked { get; set; }
+
+        public void Focus() { CheckboxInput.Focus(); }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(CheckboxControl));
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register("IsChecked", typeof(bool), typeof(CheckboxControl));
+    }
+}
---
+++ GorillaDocs/Views/Controls/ComboControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GorillaDocs.Views.Controls
+{
+    public partial class ComboControl : UserControl
+    {
+        public ComboControl() { InitializeComponent(); }
+
+        public string Label { get; set; }
+        public string Text { get; set; }
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+        public object SelectedItem { get; set; }
+        public bool IsEditable { get; set; }
+
+        public void Focus() { ComboInput.Focus(); }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(ComboControl));
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(ComboControl));
+        public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(ComboControl), new PropertyMetadata(OnItemsSourcePropertyChanged));
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register("SelectedItem", typeof(object), typeof(ComboControl));
+        public static readonly DependencyProperty IsEditableProperty = DependencyProperty.Register("IsEditable", typeof(bool), typeof(ComboControl));
+
+        static void OnItemsSourcePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var control = sender as ComboControl;
+            if (control != null)
+                control.OnItemsSourceChanged((IEnumerable)e.OldValue, (IEnumerable)e.NewValue);
+        }
+
+        void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+        {
+            // Remove handler for oldValue.CollectionChanged
+            var oldValueINotifyCollectionChanged = oldValue as INotifyCollectionChanged;
+            if (null != oldValueINotifyCollectionChanged)
+                oldValueINotifyCollectionChanged.CollectionChanged -= newValueINotifyCollectionChanged_CollectionChanged;
+            // Add handler for newValue.CollectionChanged (if possible)
+            var newValueINotifyCollectionChanged = newValue as INotifyCollectionChanged;
+            if (null != newValueINotifyCollectionChanged)
+                newValueINotifyCollectionChanged.CollectionChanged += newValueINotifyCollectionChanged_CollectionChanged;
+        }
+
+        void newValueINotifyCollectionChanged_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //Do your stuff here.
+        }
+    }
+}
