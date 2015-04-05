e9ed7409e3da9ab1ffecea51671f12bc8cda5fb5
Matthew Fitzmaurice <matthew.fitzmaurice@gmail.com>
2015-04-05 10:38:35 +0800

    WPF styles in external file

---
+++ GorillaDocs/Views/Controls/DateControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GorillaDocs.Views.Controls
+{
+    public partial class DateControl : UserControl
+    {
+        public DateControl() { InitializeComponent(); }
+
+        public string Label { get; set; }
+        public string Value { get; set; }
+
+        public void Focus() { DateInput.Focus(); }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(DateControl));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(DateTime), typeof(DateControl));
+    }
+}
